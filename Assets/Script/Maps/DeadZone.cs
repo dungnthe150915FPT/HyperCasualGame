b2bdb533d799659442e28367f25d49316ac7868b
quanvhmhe150859 <quanvhmhe150859@fpt.edu.vn>
2023-07-17 20:06:26 +0700

    zone, key, wall

--- Assets/Script/Maps/DeadZone.cs
+++ Assets/Script/Maps/DeadZone.cs
@@ -15,4 +15,12 @@ public class DeadZone : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Dead");
+        }
+    }
 }
