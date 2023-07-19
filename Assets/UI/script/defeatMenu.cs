96624d61cc3360b21f197918cd4aa49183725bb0
BaOai.Work <93336473+BaOai305@users.noreply.github.com>
2023-07-19 15:12:28 +0700

    ui

--- Assets/UI/script/defeatMenu.cs
+++ Assets/UI/script/defeatMenu.cs
@@ -1,19 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace UI.script
+public class DefeatMenu : MonoBehaviour
 {
-    public class DefeatMenu : MonoBehaviour
+    // Start is called before the first frame update
+    public void Home()
     {
-        // Start is called before the first frame update
-        public void Home()
-        {
-            SceneManager.LoadScene("MainMenu");
-        }
-        public void Restart()
-        {
-            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));
-            Time.timeScale = 1;
-        }
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));
+        Time.timeScale = 1;
     }
 }
--- Assets/UI/script/pauseMenu.cs
+++ Assets/UI/script/pauseMenu.cs
@@ -1,35 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace UI.script
+public class PauseMenu : MonoBehaviour
 {
-    public class PauseMenu : MonoBehaviour
+    // Start is called before the first frame update
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
     {
-        [SerializeField] GameObject pauseMenu;
-        
-        public void Pause()
-        {
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0;
-        }
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
 
-        public void Home()
-        {
-            SceneManager.LoadScene("MainMenu");
-            Time.timeScale = 1;
-        }
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
 
-        public void Resume()
-        {
-            pauseMenu.SetActive(false);
-            Time.timeScale = 1;
-            
-        }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
 
-        public void Restart()
-        {
-            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));
-            Time.timeScale = 1;
-        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));
+        Time.timeScale = 1;
     }
 }
