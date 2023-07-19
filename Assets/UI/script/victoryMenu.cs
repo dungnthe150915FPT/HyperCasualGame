e6230187e2fe5548b965e5afaa37c12fe731699d
Trung Nguyen <john.doe@example.com>
2023-07-19 14:32:15 +0700

    19/7

--- Assets/UI/script/victoryMenu.cs
+++ Assets/UI/script/victoryMenu.cs
@@ -3,12 +3,11 @@ using UnityEngine.SceneManagement;
 
 namespace UI.script
 {
-    public class Win : MonoBehaviour
+    public class VictoryMenu : MonoBehaviour
     {
-        // Start is called before the first frame update
         public void Home()
         {
-            SceneManager.LoadScene("UI/scene/MainMenu");
+            SceneManager.LoadScene("MainMenu");
             Time.timeScale = 1;
         }
         public void Restart()
@@ -21,34 +20,34 @@ namespace UI.script
         {
             // Kiểm tra nếu còn màn chơi tiếp theo
             if (HasNextLevel())
-        {
-            // Lấy tên cảnh của màn chơi tiếp theo
-            string nextLevelName = GetNextLevelName();
+            {
+                // Lấy tên cảnh của màn chơi tiếp theo
+                string nextLevelName = GetNextLevelName();
 
-            // Chuyển đến cảnh màn chơi tiếp theo
-            SceneManager.LoadScene(nextLevelName);
+                // Chuyển đến cảnh màn chơi tiếp theo
+                SceneManager.LoadScene(nextLevelName);
+            }
+            else
+            {
+                // Nếu không còn màn chơi tiếp theo, quay về màn hình chính
+                SceneManager.LoadScene("MainMenu");
+            }
         }
-    else
-    {
-        // Nếu không còn màn chơi tiếp theo, quay về màn hình chính
-        SceneManager.LoadScene("MainMenu");
-    }
-}
 
-private bool HasNextLevel()
-{
-    // Kiểm tra xem còn màn chơi tiếp theo hay không
-    // Ví dụ: Kiểm tra xem có một cảnh với tên "Level2" hay không
-    // Đảm bảo thay đổi kiểm tra phù hợp với cách bạn tổ chức màn chơi trong trò chơi của bạn
-    return SceneManager.GetSceneByName("Level2") != null;
-}
+        private bool HasNextLevel()
+        {
+            // Kiểm tra xem còn màn chơi tiếp theo hay không
+            // Ví dụ: Kiểm tra xem có một cảnh với tên "Level2" hay không
+            // Đảm bảo thay đổi kiểm tra phù hợp với cách bạn tổ chức màn chơi trong trò chơi của bạn
+            return SceneManager.GetSceneByName("Level2") != null;
+        }
 
-private string GetNextLevelName()
-{
-    // Lấy tên của màn chơi tiếp theo
-    // Ví dụ: Trả về tên cảnh "Level2"
-    // Đảm bảo thay đổi giá trị trả về phù hợp với tên cảnh mà bạn muốn chuyển đến
-    return "Level2";
-}
+        private string GetNextLevelName()
+        {
+            // Lấy tên của màn chơi tiếp theo
+            // Ví dụ: Trả về tên cảnh "Level2"
+            // Đảm bảo thay đổi giá trị trả về phù hợp với tên cảnh mà bạn muốn chuyển đến
+            return "Level2";
+        }
     }
 }
